d90c44a7beffa6738c841107ea09e532108c33c4
1221209283 <1221209283@student.mmu.edu.my>
2024-05-19 22:13:49 +0800

    Add main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <array> // I like vector bcs it not fixed
+
+using namespace std; //Since we can only use std only, i allow for using namespace;
+
+/* OS specified call */ // I still need to refer to lecture
+
+namespace Kawaii {
+  /* include your inside custom namespace for safety reason;
+   * Do not create function outside the namespace then include inside; (why would you do that!?)
+   * void Kawaii::function(){} is a big no no when you can put it inside!! (don't make stuff complex)
+   * Valid function will pull out of namespace.
+  */
+}
+
+int main(int argc, char* argv){
+  return 0;
+}
