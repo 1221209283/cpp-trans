008bd78b0f277b50403962ed3c22f24748b22a60
1221209283 <1221209283@student.mmu.edu.my>
2024-05-24 12:13:09 +0800

    Further Admin development

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ namespace Kawaii {
       choice.erase();
       getline(cin,choice);
     }
-    for(char& i:choice){
+    for(const char& i:choice){
       switch(i){
         case '\n':
         case ' ':
@@ -57,9 +57,9 @@ namespace Kawaii {
 
   class Person {
     protected:
-      bool legacy_sex;
+      bool legacy_sex = false;
       string name;
-      unsigned short age;
+      unsigned short age = 0;
 
       void change_name();
 
@@ -70,6 +70,21 @@ namespace Kawaii {
       {}
 
       ~Person() = default;
+
+      /*accessor*/
+
+      bool Sex() const {
+        return legacy_sex;
+      }
+
+      string Name() const {
+        return name;
+      }
+
+      unsigned short Age() const {
+        return age;
+      }
+
   };
 
   class Customer : public Person {
@@ -78,47 +93,117 @@ namespace Kawaii {
   };
 
   class Admin : public Person {
-      char status;
+      char status = '\0';
     public:
-      Admin() = default;
+
+      Admin(const bool x) {
+        if(!x) return;
+        change_sex();
+        change_name();
+        change_age();
+      }
       Admin(const bool& legacy_sex,const string& name,const unsigned short& age,const char& status) { // init list to modify local class not inherited class
         this->legacy_sex = legacy_sex;
         this->name = name;
         this->age = age;
         this->status = status;
       }
-      ~Admin() = default;
+      ~Admin() {
+        cout << "Admin : " << name << " Destroyed !" << endl;
+      }
 
       int change_name(){
-        do{
-          cout << "Insert Name # "; //user input flow will be revised!!
-          getline(cin,name);
-          cout << "Confirm !? [Y|n] # ";
-        }while (!choice());
+        cout << "Insert Name : "; //user input flow will be revised!!
+        getline(cin,name);
         return 0;
       }
 
       int change_age() {
-        do{
-          cout << "Insert Age # ";
-          if(!(cin >> age)){ // revise here very very important!!!
-            cin.clear();
-          }
+        cout << "Insert Age : ";
+        if(!(cin >> age)){ // revise here very very important!!!
+          cerr << "Error Input!!" << endl;
+          cin.clear();
           cin.ignore(MAX_STREAM_SIZE,'\n');
-          cout << "Confirm changes !? [Y|n] # ";
-        }while(!choice());
+          return -1;
+        } else cin.ignore(MAX_STREAM_SIZE,'\n');
         return 0;
       }
 
+      int change_sex() {
+        unsigned short good = 0, bad = 0;
+        string input;
+        cout << "Insert Sex [F/m] : ";
+        getline(cin,input);
+        for(const char& i:input){
+          switch(i) {
+            case '\n':
+            case ' ':
+            case '\t':
+            case '\r':
+            case '\f':
+            case '\v':
+              break;
+            case '0':
+            case 'F':
+            case 'f':
+              good++;
+              break;
+            case '1':
+            case 'M':
+            case 'm':
+              bad++;
+              break;
+            default:
+              cerr << "Error Input!!" << endl;
+              return -1;
+          }
+        }
+        if((good + bad) > 1) {
+          cerr << "Error Gender!!" << endl;
+          return -1;
+        }
+        if((good + bad) == 0) good++;
+        legacy_sex = (good == 0 && bad == 1);
+        return 0;
+      }
+
+      int change_status() {
+        cout << "Insert Status : ";
+        return 0;
+      } 
+
+      void change_sex(const bool& legacy_sex){
+        this->legacy_sex = legacy_sex;
+      }
       void change_name(const string& name){
         this->name = name;
       }
+      void change_age(const unsigned short& age){
+        this->age = age;
+      }
+      void change_status(const char& status){
+        this->status = status;
+      }
+
+      /*accessor*/
+
+      char Status() const {
+        return status;
+      }
+
+      void Details() const {
+        cout << "=Admin=" << '\n'
+          << "Name : " << name << '\n'
+          << "Sex  : " << ((legacy_sex == false) ? "Female" : "Male") << '\n'
+          << "Age  : " << age << '\n'
+          << "Status " << status << endl;
+      }
+
   };
 }
 
 int main(int argc, char* argv[]){
-  Kawaii::Admin a;
-  (void) a.change_name();
-  (void) a.change_age();
+  Kawaii::Admin a(true);
+  a.Details();
   return 0;
 }
