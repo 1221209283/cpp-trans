42b889b88752476ae4d184399e624436e38aa6b9
1221209283 <1221209283@student.mmu.edu.my>
2024-06-12 02:47:50 +0800

    user number type safe (num_input)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include <cstdio>
 #include <iostream>
-//#include <string>
 #include <limits>
 #include <string>
+#include <type_traits>
 
 /* OS specified call */
 #if _WIN32
@@ -17,6 +16,7 @@
 #endif
 
 //#error "Comment if you reviewed the code!! \n I dont want faulty code!!"
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 using namespace std;
 
@@ -144,7 +144,7 @@ void display(const unsigned short& person_num, const customer* const arry_cust) 
 
 //function covert num
 
-template <class T>
+template <typename T>
 void int_input(T& a,string&& out = ""){
   bool error_input = false, no_symbol = false;
   string input;
@@ -183,7 +183,7 @@ void int_input(T& a,string&& out = ""){
   a = (T)stoi(input);
 }
 
-template <class T>
+template <typename T>
 void unsign_input(T& a,string&& out = ""){
   bool error_input = false;
   string input;
@@ -211,7 +211,7 @@ void unsign_input(T& a,string&& out = ""){
   a = (T)stoull(input);
 }
 
-template <class T>
+template <typename  T>
 void float_input(T& a,string&& out = ""){
   bool error_input = false, no_symbol = false, dot_format = false, lock_dot = false;
   string input;
@@ -255,6 +255,89 @@ void float_input(T& a,string&& out = ""){
   }while(error_input);
   a = (T)stod(input);
 }
+
+template <typename T>
+void num_input(T& a,string&& out = "input : "){ // one big bloat function
+  static_assert(!is_unsigned_v<T>, "This Function cannot handle unsigned varaible !!");
+  bool error_input = false, no_symbol = false, dot_format = false, lock_dot = false, is_null = true;
+  string input;
+  do {
+    error_input = false;
+    no_symbol = false;
+    dot_format = false;
+    lock_dot = false;
+    is_null = true;
+    input.erase();
+    cout << out;
+    getline(cin, input);
+    for(const char& i:input){
+      if((int)i < 33) continue; //white space
+      if((int)i < 58 && (int)i > 47){ // check is number
+        no_symbol = true;
+        dot_format = true;
+        is_null = false;
+        continue;
+      }
+      switch (i) {
+        case '+':
+        case '-':
+          if(!no_symbol){ // make sure plus minus is on the first
+            no_symbol = true;
+          }else{
+            error_input = true;
+          }
+          break;
+        case '.':
+          if(dot_format && !lock_dot){ // make sure only one dot between number
+            lock_dot = true;
+            dot_format = false;
+          }else{
+            error_input = true;
+          }
+          break;
+        default:
+          error_input = true; // other words
+      }
+      if(error_input){
+        cerr << "\e[31mError Input!!\e[0m" << endl;
+        //cerr << no_symbol << dot_format << lock_dot << is_null << endl;
+        break;
+      }
+    }
+    if(is_null){
+      error_input = true; // check the string is empty that only contain whitespace
+      continue;
+    }
+    if(!dot_format){ // prevent empty number after .
+      cerr << "\e[31mError Input!!\e[0m" << endl;
+      error_input = true;
+    }
+  }while (error_input);
+  if(is_integral_v<T>){
+    a = (T)stoi(input);
+    return;
+  }
+  if(is_same_v<T, float>){
+    a = stof(input);
+    return;
+  }
+  if(is_same_v<T, double>){
+    a = stod(input);
+    return;
+  }
+}
+
+/*
+About the Big num_input above is very Big.
+I wiil try to optimised a bit.
+the fucntion is developed and inspired by cpu CNZV
+it does have a similiar boolen to check user input format flow and format.
+i design this to make sure user can a correct input,
+not: 
+.2
+2.
+other false inputs
+*/
 
 // end num convert
 
