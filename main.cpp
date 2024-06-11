a47dab770be6e38c74d40d1bfeb9a5acb9aa3b97
1221209283 <1221209283@student.mmu.edu.my>
2024-06-11 19:44:51 +0800

    Add safe user_input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <cstdio>
 #include <iostream>
 //#include <string>
 #include <limits>
+#include <string>
 
 /* OS specified call */
 #if _WIN32
@@ -31,8 +33,30 @@ class customer {
   }
 
   void set_age() {
-    cin >> age;
-    cin.ignore(MAX_STREAM_SIZE,'\n');
+    bool error_input = false;
+    string input;
+    do{
+      error_input = false;
+      input.erase();
+      cout << "Input Age : ";
+      getline(cin,input);
+      if(input == ""){
+        error_input = true;
+        continue;
+      }
+      for(const char& i:input){
+        if((int)i < 33){
+          continue;
+        }else if((int)i < 58 && (int)i > 47){
+          continue;
+        }else{
+          cerr << "\e[31mError Age!!\e[0m" << endl;
+          error_input = true;
+          break;
+        }
+      }
+    }while(error_input);
+    age = stoi(input);
   }
 
   void set_phone() {
@@ -41,38 +65,49 @@ class customer {
 
   int set_sex() {
     unsigned short good = 0, bad = 0;
+    bool input_error = false;
     string input;
-    cout << "Insert Sex [F/m] : ";
-    getline(cin,input);
-    for(const char& i:input){
-      switch(i) {
-        case '\n':
-        case ' ':
-        case '\t':
-        case '\r':
-        case '\f':
-        case '\v':
-          break;
-        case '0':
-        case 'F':
-        case 'f':
-          good++;
-          break;
-        case '1':
-        case 'M':
-        case 'm':
-          bad++;
-          break;
-        default:
-          cerr << "Error Input!!" << endl;
-          return -1;
+    do{
+      good = 0;
+      bad = 0;
+      input_error = false;
+      cout << "Insert Sex [f/m] : ";
+      getline(cin,input);
+      for(const char& i:input){
+        switch(i) {
+          case '\n':
+          case ' ':
+          case '\t':
+          case '\r':
+          case '\f':
+          case '\v':
+            break;
+          case '0':
+          case 'F':
+          case 'f':
+            good++;
+            break;
+          case '1':
+          case 'M':
+          case 'm':
+            bad++;
+            break;
+          default:
+            cerr << "\e[31mError Input!!\e[0m" << endl;
+            input_error = true;
+        }
+        if(input_error) break;
       }
-    }
-    if((good + bad) > 1) {
-      cerr << "Error Sex!!" << endl;
-      return -1;
-    }
-    if((good + bad) == 0) good++;
+
+      if(input_error) continue;
+
+      if((good+bad) == 1);
+      else {
+        cerr << "\e[31mError Sex!!\e[0m" << endl; 
+        input_error = true;
+      }
+
+    }while(input_error);
     legacy_sex = (good == 0 && bad == 1);
     return 0;
   }
@@ -80,7 +115,6 @@ class customer {
   void input_field() {
     cout << "Name : ";
     set_name();
-    cout << "Age  : ";
     set_age();
     set_sex();
     cout << "Phone: ";
@@ -108,12 +142,127 @@ void display(const unsigned short& person_num, const customer* const arry_cust) 
 
 }
 
+//function covert num
+
+template <class T>
+void int_input(T& a,string&& out = ""){
+  bool error_input = false, no_symbol = false;
+  string input;
+  do{
+    error_input = false;
+    no_symbol = false;
+    input.erase();
+    cout << out;
+    getline(cin,input);
+    if(input == ""){
+      error_input = true;
+      continue;
+    }
+    for(const char& i:input){
+      if((int)i < 33){
+        continue;
+      }else if((int)i < 58){
+        if((int)i > 47){
+          no_symbol = true;
+          continue;
+        }else if((i == '+' || i == '-') && !no_symbol){
+          no_symbol = true;
+          continue;
+        }else{
+          cerr << "\e[31mError Int!!\e[0m" << endl;    
+          error_input = true;
+          break;
+        }
+      }else{
+        cerr << "\e[31mError Input!!\e[0m" << endl;
+        error_input = true;
+        break;
+      }
+    }
+  }while(error_input);
+  a = (T)stoi(input);
+}
+
+template <class T>
+void unsign_input(T& a,string&& out = ""){
+  bool error_input = false;
+  string input;
+  do{
+    error_input = false;
+    input.erase();
+    cout << out;
+    getline(cin,input);
+    if(input == ""){
+      error_input = true;
+      continue;
+    }
+    for(const char& i:input){
+      if((int)i < 33){
+        continue;
+      }else if((int)i < 58 && (int)i > 47){
+        continue;
+      }else{
+        cerr << "\e[31mError Input!!\e[0m" << endl;
+        error_input = true;
+        break;
+      }
+    }
+  }while(error_input);
+  a = (T)stoull(input);
+}
+
+template <class T>
+void float_input(T& a,string&& out = ""){
+  bool error_input = false, no_symbol = false, dot_format = false, lock_dot = false;
+  string input;
+  do{
+    error_input = false;
+    no_symbol = false;
+    dot_format = false;
+    lock_dot = false;
+    input.erase();
+    cout << out;
+    getline(cin,input);
+    if(input == ""){
+      error_input = true;
+      continue;
+    }
+    for(const char& i:input){
+      if((int)i < 33){
+        continue;
+      }else if((int)i < 58){
+        if((int)i > 47) {
+          no_symbol = true;
+          dot_format = true;
+          continue;
+        }else if((i == '+' || i == '-') && !no_symbol){
+          no_symbol = true;
+          continue;
+        }else if(i == '.' && dot_format && !lock_dot){
+          lock_dot = true;
+          continue;
+        }else{
+          cerr << "\e[31mError Input!!\e[0m" << endl;    
+          error_input = true;
+          break;
+        }
+      }else{
+        cerr << "\e[31mError Input!!\e[0m" << endl;
+        error_input = true;
+        break;
+      }
+    }
+  }while(error_input);
+  a = (T)stod(input);
+}
+
+// end num convert
+
 int main(int argc, char* argv[]){
   unsigned short person_num = 0; //danger out of range!!
   char vehicle_choose = '\0';
 
-  cout << "Enter number of person: ";
-  cin >> person_num;
+  unsign_input(person_num,"Enter number of person: ");
 
   customer* arry_cust = new customer[person_num];
 
